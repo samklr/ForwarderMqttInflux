fd4d8bf506b3f4ba6a14ee6cda8931d7c0cb9ee6
Sam Bessalah <samklr@users.noreply.github.com>
2016-08-07 17:39:44 +0200

    Update runner.sh
    Blah

--- runner.sh
+++ runner.sh
@@ -6,6 +6,6 @@ export JAR_HOME="target/scala-2-11/app-mqtt-assembly-1.0.jar"
 
 export GC_OPTIONS="-XX:+UseG1GC"
 
-#nohup java -Xms512m -Xmx4096m $GC_OPTIONS -server -jar $JAR_HOME > /dev/null 2>&1 &
+#nohup java -Xms512m -Xmx4096m $GC_OPTIONS -server -jar $JAR_HOME > /dev/null 
 
-java $GC_OPTIONS -Xms512m -Xmx4096m -server -jar $JAR_HOME > /dev/null 2>&1 &
+java  -server $GC_OPTIONS -Xms4096m -Xmx4096m -jar $JAR_HOME > /dev/null 2>&1 &
