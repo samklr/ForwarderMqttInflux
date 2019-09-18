070c7b645c42819e1bccc83cee84a32ca8d29aa4
Sam Bessalah <samklr@users.noreply.github.com>
2019-09-18 02:17:53 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,8 +4,6 @@ sbt/sbt assembly
 
 export JAR_HOME="target/scala-2-11/app-mqtt-assembly-1.0.jar"
 
-export GC_OPTIONS="-XX:+UseG1GC"
-
 #nohup java -Xms512m -Xmx4096m $GC_OPTIONS -server -jar $JAR_HOME > /dev/null 
 
-java  -server $GC_OPTIONS -Xms4096m -Xmx4096m -jar $JAR_HOME > /dev/null 2>&1 &
+java  -server -Xms4096m -Xmx4096m -jar $JAR_HOME > /dev/null 2>&1 &
